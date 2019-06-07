58fa762035dae82304070d64a6457d697c0d773d
Valena Del Valle <valenadelvalle4@gmail.com>
2019-06-07 19:18:11 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,40 +3,34 @@ def create_an_empty_array
 end
 
 def create_an_array
-  favorite_colors = [ "Lavender", "Blue", "Gold", "Orange" ]
+  array = [ "I", "am", "really", "learning" ]
 end
 
-def add_element_to_end_of_array(array, element)
-  array = ["I", "am", "really", "learning" ]
-end
-
-def add_element_to_start_of_array(array, element)
-  array = ["wow", "I", "Am", "Really", "Learning"]
+def add_element_to_end_of_array(array, "#{arrays!}")
   element = "arrays!"
 end
 
+def add_element_to_start_of_array(array, element)
+  array << "wow"
+end
+
 def remove_element_from_end_of_array(array)
-  array = ["Wow", "I", "am", "really", "learning" ]
   element = "arrays!"
 end
 
 def remove_element_from_start_of_array(array)
-  array = ["I", "am", "really", "learning", "arrays!"]
   element = "wow"
 end
 
 def retrieve_element_from_index(array, index_number)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
   index_number = 2
   element = "am"
 end
 
 def retrieve_first_element_from_array(array)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
   element = "wow"
 end
 
 def retrieve_last_element_from_array(array)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
   element = "arrays!"
 end
