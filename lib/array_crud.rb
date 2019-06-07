0a92cdc5f7c022d54fd94afe64ff702f25c45f0c
Valena Del Valle <valenadelvalle4@gmail.com>
2019-06-07 19:07:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,6 +8,7 @@ end
 
 def add_element_to_end_of_array(element)
   element << "arrays!"
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
 end
 
 def add_element_to_start_of_array(element)
